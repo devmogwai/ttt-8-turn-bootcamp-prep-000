9a164ae246581935bfdbf3500931cf6e830949bc
Daniel Doughty <djdoughty@gmail.com>
2018-04-17 21:42:13 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -2,6 +2,16 @@ def display_board (board)
   puts board
 end
 
+def position_taken?(board, index)
+  if board[index] == "" || board[index] == " " || board[index] == nil
+    return false
+  end
+  
+  if board[index] == "X" || board[index] == "O"
+    return true
+  end
+end
+
 def valid_move? (board, index)
    return !position_taken?(board, index) && index.between?(0,8)
 end
